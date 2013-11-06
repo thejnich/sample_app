d10f39aecde86d44ba57a39d1b38784966f6b95e
Jeff Nicholson <jeff@thejnich.com>
2013-11-06 13:34:48 -0700

    Refactor out common base title in spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+   let(:base_title) { "Ruby on Rails Tutorial Sample App | " }
+
    describe "Home page" do
 
       it "should have the content 'Sample App'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
       it "should have the title 'Home'" do
          visit '/static_pages/home'
-         expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+         expect(page).to have_title("#{base_title}Home")
       end
 
    end
@@ -25,7 +27,7 @@ describe "Static pages" do
 
       it "should have the title 'Help'" do
          visit '/static_pages/help'
-         expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+         expect(page).to have_title("#{base_title}Help")
       end
 
    end
@@ -39,7 +41,7 @@ describe "Static pages" do
 
       it "should have the title 'About'" do
          visit '/static_pages/about'
-         expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+         expect(page).to have_title("#{base_title}About")
       end
 
    end
@@ -53,10 +55,9 @@ describe "Static pages" do
 
       it "should have the title 'Contact'" do
          visit '/static_pages/contact'
-         expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+         expect(page).to have_title("#{base_title}Contact")
       end
 
    end
-
 
 end
