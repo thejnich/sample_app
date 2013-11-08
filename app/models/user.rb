48eb3240b7357622e278ce623baca47e414bb7f9
Jeff Nicholson <jeff@thejnich.com>
2013-11-08 10:45:50 -0700

    Construct data model for following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
    has_many :microposts, dependent: :destroy
+   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+   has_many :followed_users, through: :relationships, source: :followed
+   has_many :reverse_relationships, foreign_key: "followed_id",
+                                    class_name:  "Relationship",
+                                    dependent:   :destroy
+   has_many :followers, through: :reverse_relationships, source: :follower
    validates :name, presence: true, length: { maximum: 50 }
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
    
@@ -24,9 +30,21 @@ class User < ActiveRecord::Base
       Micropost.where("user_id = ?", id)
    end
 
-   private
-   def create_remember_token
-      self.remember_token = User.encrypt(User.new_remember_token)
+   def following?(other_user)
+      self.relationships.find_by(followed_id: other_user.id)
    end
+
+   def follow!(other_user)
+      self.relationships.create!(followed_id: other_user.id)
+   end
+
+   def unfollow!(other_user)
+      self.relationships.find_by(followed_id: other_user.id).destroy!
+   end
+
+   private
+      def create_remember_token
+         self.remember_token = User.encrypt(User.new_remember_token)
+      end
 
 end
